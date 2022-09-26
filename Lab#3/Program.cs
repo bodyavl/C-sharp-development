754b7a075d9468025fbc26522ddb03c3159efc7d
bodyavl <bodyavl228@gmail.com>
2022-09-26 19:13:22 +0300

    Lab3 added

---
+++ Lab#3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab3_university
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            string? strNum;
+            bool numIsValid = false;
+            do
+            {
+                Console.WriteLine("Enter number");
+                strNum = Console.ReadLine();
+                numIsValid = int.TryParse(strNum, out num);
+                if (numIsValid) break;
+                Console.WriteLine("Enter a valid number");
+            } while (!numIsValid);
+
+            //перевірка чи число непарне і тризначне
+            bool numIsWhatWeNeed = num % 2 != 0 && strNum?.Length == 3;
+
+            Console.WriteLine(numIsWhatWeNeed);
+        }
+    }
+}
